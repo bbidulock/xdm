26392a977dbb5a6590d740236d862656d333e68b
Alan Coopersmith <Alan.Coopersmith@sun.com>
2004-03-27 06:13:23 +0000

    67. Prevent segfault if LISTEN specified with no hosts in xdm access     control file. (Reported by Peter Breitenlohner to devel@xfree86.org)     (Alan Coopersmith) 66. Typo in Xau.man reported by Peter Breitenlohner to devel@xfree86.org     (Alan Coopersmith)

--- access.c
+++ access.c
@@ -1,5 +1,5 @@
 /*
- * $XdotOrg: access.c,v 1.5 2001/02/09 02:05:40 xorgcvs Exp $
+ * $XdotOrg: xc/programs/xdm/access.c,v 1.1.4.3.4.1 2004/03/04 17:48:55 eich Exp $
  * $Xorg: access.c,v 1.5 2001/02/09 02:05:40 xorgcvs Exp $
  *
 Copyright 1990, 1998  The Open Group
@@ -904,9 +904,9 @@ void ForEachListenAddr (
 	    h = d->hosts;
 	    if (h != NULL) {
 		(*listenfunction) (&h->entry.hostAddress, closure);
-	    }
-	    for (h = h->next; h != NULL; h = h->next) {
-		(*mcastfunction) (&h->entry.hostAddress, closure);
+		for (h = h->next; h != NULL; h = h->next) {
+		    (*mcastfunction) (&h->entry.hostAddress, closure);
+		}
 	    }
 	}
     }
