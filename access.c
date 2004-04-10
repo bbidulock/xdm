72a126d2b73d66c253d50ac8d1ab95afe15d85a1
Roland Mainz <roland.mainz@nrubsig.org>
2004-04-10 09:05:49 +0000

    Resync to 2004-04-10 XORG-RELEASE-1 branch

--- access.c
+++ access.c
@@ -1,5 +1,5 @@
 /*
- * $XdotOrg: access.c,v 1.5 2001/02/09 02:05:40 xorgcvs Exp $
+ * $XdotOrg: xc/programs/xdm/access.c,v 1.1.4.3.4.2 2004/03/27 06:13:23 alanc Exp $
  * $Xorg: access.c,v 1.5 2001/02/09 02:05:40 xorgcvs Exp $
  *
 Copyright 1990, 1998  The Open Group
@@ -904,9 +904,9 @@ void ForEachListenAddr (
 	    h = d->hosts;
 	    if (h != NULL) {
 		(*listenfunction) (&h->entry.hostAddress, closure);
-	    }
-	    for (h = h->next; h != NULL; h = h->next) {
-		(*mcastfunction) (&h->entry.hostAddress, closure);
+		for (h = h->next; h != NULL; h = h->next) {
+		    (*mcastfunction) (&h->entry.hostAddress, closure);
+		}
 	    }
 	}
     }
