bb09e97d5d308f7f46c46b9f94c9755c2e635395
Alexander Gottwald <alexander.gottwald@s1999.tu-chemnitz.de>
2005-02-11 10:58:38 +0000

    Import changes from XORG-6.8.2

--- resource.c
+++ resource.c
@@ -1,4 +1,4 @@
-/* $XdotOrg: xc/programs/xdm/resource.c,v 1.2 2004/04/23 19:54:42 eich Exp $ */
+/* $XdotOrg$ */
 /* $Xorg: resource.c,v 1.4 2001/02/09 02:05:40 xorgcvs Exp $ */
 /*
 
@@ -237,7 +237,7 @@ struct dmResources {
 #if !defined(ARC4_RANDOM)
 { "randomFile",	"RandomFile",	DM_STRING,	&randomFile,
 				DEF_RANDOM_FILE} ,
-{ "prgndSocket", "PrngdSocket", DM_STRING,	&prngdSocket, 
+{ "prngdSocket", "PrngdSocket", DM_STRING,	&prngdSocket, 
 				DEF_PRNGD_SOCKET},
 { "prngdPort", "PrngdPort",	DM_INT,		(char **) &prngdPort,
 				DEF_PRNGD_PORT},
