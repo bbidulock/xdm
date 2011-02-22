bbidulock/xdm

commit f8585c60831a8e5ddebce18bdd7e78d217a822c5

X.Org Bug #10992: xlogin crashes if window size is too small
X.Org Bugzilla #10992 <https://bugs.freedesktop.org/show_bug.cgi?id=10992>

greeter/Login.c
@@ -402,14 +402,14 @@ realizeValue (LoginWidget w, int cursor, int promptNum, GC gc)
 	    offset = VALUE_SHOW_START(w, promptNum);
 	    textlen = strlen (text + offset);
 
-	    do
+	    while ((textlen > 0) && (textwidth > width))
 	    {
 		if (offset < PROMPT_CURSOR(w, promptNum)) {
 		    offset++;
 		}
 		textlen--;
 		textwidth = TEXT_WIDTH (text, text + offset, textlen);
-	    } while ((textlen > 0) && (textwidth > width));
+	    } 
 
 	    VALUE_SHOW_START(w, promptNum) = offset;
 	    VALUE_SHOW_END(w, promptNum) = offset + textlen;

commit f59d8f32c755b8747acf71aa3ab4c4809bff4ebf

greeter: Make sure text input areas stay inside the lines
Regression introduced by f8585c60831a8e5ddebce18bdd7e78d217a822c5
broke bounds checking for text displayed in input fields.

Since this makes each TEXT_WIDTH value only be used once, just use
them directly instead of storing the result in a variable.

Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>

greeter/Login.c
@@ -404,24 +404,21 @@ realizeValue (LoginWidget w, int cursor, int promptNum, GC gc)
     } else if ((state == LOGIN_PROMPT_ECHO_ON) || (state == LOGIN_TEXT_INFO) ||
 	       ((state == LOGIN_PROMPT_ECHO_OFF) && (w->login.echo_passwd == True)))
     {
-	int textwidth;
 	int offset = max(cursor, VALUE_SHOW_START(w, promptNum));
 	int textlen = strlen (text + offset);
 
-	textwidth = TEXT_WIDTH (text, text + offset, textlen);
-
-	if (textwidth > (width - curoff)) {
+	if (TEXT_WIDTH (text, text + offset, textlen) > (width - curoff)) {
 	    /* Recalculate amount of text that can fit in field */
 	    offset = VALUE_SHOW_START(w, promptNum);
 	    textlen = strlen (text + offset);
 
-	    while ((textlen > 0) && (textwidth > width))
+	    while ((textlen > 0) &&
+		   (TEXT_WIDTH (text, text + offset, textlen) > width))
 	    {
 		if (offset < PROMPT_CURSOR(w, promptNum)) {
 		    offset++;
 		}
 		textlen--;
-		textwidth = TEXT_WIDTH (text, text + offset, textlen);
 	    }
 
 	    VALUE_SHOW_START(w, promptNum) = offset;