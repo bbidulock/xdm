7a56946260616f7f5ff43ecbaac1f2ada9b0afcd
Chí-Thanh Christopher Nguyễn <chithanh@gentoo.org>
2014-01-06 15:52:47 +0000

    xdm uses setproctitle without proper includes for Linux libbsd
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>

--- xdm/session.c
+++ xdm/session.c
@@ -51,6 +51,15 @@ from The Open Group.
 #include <ctype.h>
 #include <grp.h>	/* for initgroups */
 
+# ifdef HAVE_SETPROCTITLE
+#  include <sys/types.h>
+#  ifdef __linux__
+#   include <bsd/unistd.h>
+#  else
+#   include <unistd.h>
+#  endif
+# endif
+
 #ifndef USE_PAM        /* PAM modules should handle these */
 # ifdef SECURE_RPC
 #  include <rpc/rpc.h>
